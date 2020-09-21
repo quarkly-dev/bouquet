fd368e8ab42db077cc86d10134ad9f4978da83a7
quarkly-dev <dev@quarkly.io>
2020-09-21 11:12:23 +0300

    Some changes

---
+++ qapi.js
@@ -0,0 +1,70 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5eba3f5f46cf7b001ed137c9",
+				"5eba438946cf7b001ed137f2",
+				"5eba437d46cf7b001ed137ee",
+				"5eba439246cf7b001ed137f6",
+				"5eba439746cf7b001ed137fa",
+				"5eba439c46cf7b001ed137fe",
+				"5f4671bd6724f4001e384823"
+			]
+		},
+		"5eba3f5f46cf7b001ed137c9": {
+			"id": "5eba3f5f46cf7b001ed137c9",
+			"name": "index",
+			"pageUrl": "index",
+			"seo": {
+				"title": "",
+				"description": "",
+				"og:title": "",
+				"og:description": "",
+				"og:img": "https://images.unsplash.com/photo-1558981806-ec527fa84c39?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0MjIzN30"
+			}
+		},
+		"5eba437d46cf7b001ed137ee": {
+			"id": "5eba437d46cf7b001ed137ee",
+			"pageUrl": "services",
+			"name": "Services",
+			"children": []
+		},
+		"5eba438946cf7b001ed137f2": {
+			"id": "5eba438946cf7b001ed137f2",
+			"pageUrl": "portfolio",
+			"name": "Portfolio"
+		},
+		"5eba439246cf7b001ed137f6": {
+			"id": "5eba439246cf7b001ed137f6",
+			"pageUrl": "workshops",
+			"name": "Workshops"
+		},
+		"5eba439746cf7b001ed137fa": {
+			"id": "5eba439746cf7b001ed137fa",
+			"pageUrl": "blog",
+			"name": "Blog",
+			"styles": {
+				"background": "--secondaryGradient"
+			}
+		},
+		"5eba439c46cf7b001ed137fe": {
+			"id": "5eba439c46cf7b001ed137fe",
+			"pageUrl": "contact",
+			"name": "Contact",
+			"styles": {
+				"background": "#4a1616",
+				"text-decoration-line": "",
+				"color": "#953a3a"
+			}
+		},
+		"5f4671bd6724f4001e384823": {
+			"id": "5f4671bd6724f4001e384823",
+			"pageUrl": "404",
+			"name": "404"
+		}
+	},
+	"projectType": "gatsby"
+}
